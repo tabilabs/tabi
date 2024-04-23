04ab443754b8f00b40ddf976d30bb31529f10a52
oris <taramaoriskykage@gmail.com>
2024-04-23 15:19:17 +0800

    feat: exported interfaces

--- x/captains/keeper/epoch.go
+++ x/captains/keeper/epoch.go
@@ -1,11 +1,14 @@
 package keeper
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
 // epochs handle stage transitions in an epoch.
 
-func (k Keeper) GetEpochs() {
+// PruneEpochs prunes useless state on epochs.
+func (k Keeper) PruneEpochs(ctx sdk.Context) {
 	panic("not implemented")
 }
 
-func (k Keeper) PruneEpochs() {
-	panic("not implemented")
+func (k Keeper) GetCurrentEpoch(ctx sdk.Context) uint64 {
+	panic("implement me")
 }
