086bc487879ef28f2cb7e43ed4dd57fd1b2213b4
oris <taramaoriskykage@gmail.com>
2024-04-30 14:31:12 +0800

    revise exported interface in captains

--- x/claims/types/expected_keepers.go
+++ x/claims/types/expected_keepers.go
@@ -59,13 +59,10 @@ type CaptainsKeeper interface {
 	// GetCurrentEpoch return the current epoch id.
 	GetCurrentEpoch(ctx sdk.Context) uint64
 
-	// GetNodeHistoricalEmissionOnEpoch returns the historical mission of the node at the end of epoch.
-	GetNodeHistoricalEmissionOnEpoch(ctx sdk.Context, epochID uint64, nodeID string) sdk.Dec
+	// CalAndGetNodeHistoricalEmissionOnEpoch returns the historical mission of the node at the end of epoch.
+	CalAndGetNodeHistoricalEmissionOnEpoch(ctx sdk.Context, epochID uint64, nodeID string) sdk.Dec
 
 	// GetNodeHistoricalEmissionOnLastClaim returns the historical emission the last time user claimed.
-	// That says, if the user claims on epoch(T+1), the user will get rewards accrued at the end of epoch(T).
-	// The next time the user claims, let's say on epoch(T+k+1), we can easily calc the rewards by subtracting
-	// node_historical_emission_on_last_claim from node_historical_emission(T+k)
 	GetNodeHistoricalEmissionOnLastClaim(ctx sdk.Context, nodeID string) sdk.Dec
 
 	// UpdateNodeHistoricalEmissionOnLastClaim updates the node_historical_emission_on_last_claim.
